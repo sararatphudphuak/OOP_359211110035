c65c47253b069e9e479c769ea44eb8a4c453d6dc
sararatphudphuak <diizampkup@gmail.com>
2017-11-27 16:22:10 +0700

    Tese CalGrade

---
+++ src/ooplab3/CalGrade.java
@@ -0,0 +1,24 @@
+package ooplab3;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CalGrade {
+    //    score 0-49 grade F
+//    score 50-59 grade D
+//    score 60-69 grade C
+//    score 70-79 grade B
+//    score 80-100 grade A
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter your Score");
+        int score = Integer.parseInt(reader.readLine());
+
+        if(score<50) System.out.println("Your grade: F");
+        else if (score<=59) System.out.println("Your grade: D");
+        else if (score<=69) System.out.println("Your grade: C");
+        else if (score<=79) System.out.println("Your grade: B");
+        else System.out.println("Your grade: A");
+
+    }
+}
