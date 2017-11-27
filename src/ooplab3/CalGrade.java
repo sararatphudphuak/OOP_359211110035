1a26c5d158f4309ca428163e4f6c02b0410d05dd
sararatphudphuak <diizampkup@gmail.com>
2017-11-27 16:41:59 +0700

    Tese CalGrade

--- src/ooplab3/CalGrade.java
+++ src/ooplab3/CalGrade.java
@@ -11,14 +11,24 @@ public class CalGrade {
 //    score 80-100 grade A
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter your Score");
-        int score = Integer.parseInt(reader.readLine());
+        int count =0;
+        int score =0;
+        for(int i=1;i<=3;i++) {
+            System.out.print("Enter your Score (1-100) : ");
+            score = Integer.parseInt(reader.readLine());
+            if ((score < 0 || score > 100)) {
+                count++;
+                if (count == 3) {
+                    System.out.println("System Error");
+                    break;
+                }
+            } else {
 
-        if(score<50) System.out.println("Your grade: F");
-        else if (score<=59) System.out.println("Your grade: D");
-        else if (score<=69) System.out.println("Your grade: C");
-        else if (score<=79) System.out.println("Your grade: B");
-        else System.out.println("Your grade: A");
+                if (score < 50) System.out.println("Your grade: F");
+                else if (score <= 59) System.out.println("Your grade: D");
+                else if (score <= 69) System.out.println("Your grade: C");
+                else if (score <= 79) System.out.println("Your grade: B");
+                else System.out.println("Your grade: A");
 
-    }
-}
+            }
+        }
