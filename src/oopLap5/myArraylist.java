534cb80fdef008642868c27ee667167ae69f2e0d
sararatphudphuak <diizampkup@gmail.com>
2017-12-25 16:13:02 +0700

    Array

---
+++ src/oopLap5/myArraylist.java
@@ -0,0 +1,15 @@
+package oopLap5;
+
+import java.util.ArrayList;
+
+public class myArraylist {
+    public static void main (String[] args){
+        ArrayList mtList = new ArrayList();
+        //add method
+        myList.add("AMP");
+        System.out.println(myList);
+        myList.add("PRAE");
+        malist.add("AORN");
+
+    }//main
+}//class
